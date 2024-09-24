9dbe72ad2b5934511099a13d280fd6e13f64cd81
Sasha.d <84565787+Spahme@users.noreply.github.com>
2024-09-24 16:43:15 +0200

    modifier rdv

--- app/src/core/services/rdv/ServiceRDV.php
+++ app/src/core/services/rdv/ServiceRDV.php
@@ -33,7 +33,7 @@ class ServiceRDV implements ServiceRDVInterface
             return $rdv->toDTO($praticien);
     }
 
-    /*string $praticienID, $patientID, string $specialite, \DateTimeImmutable $dateHeure*/
+    /*string $praticienID, string $patientID, string $specialite, \DateTimeImmutable $dateHeure*/
     public function creerRendezvous(string $id, string $praticienID, string $patientID, string $specialite, \DateTimeImmutable $dateHeure) : RdvDTO {
         $rdv = new RendezVous($praticienID, $patientID, $specialite, $dateHeure);
         $rdv->setID($id);
@@ -56,13 +56,50 @@ class ServiceRDV implements ServiceRDVInterface
 
     }
 
-    /*string $praticienID*/
-    public function supprimerRendezVous(string $id) : void {
+    /*string $rdvId*/
+    public function supprimerRendezVous(string $id) : RdvDTO {
         try {
             $rdv = $this->rdvRepository->getRDVById($id);
-            $this->rdvRepository->delete($id);
+            $this->rdvRepository->delete($rdv);
         } catch(RepositoryEntityNotFoundException $e) {
             throw new ServiceRDVInvalidDataException('invalid RDV ID');
+        } 
+    }
+
+    /* string $id, string $praticienId, string $patientId, string $specialite, \DateTimeImmutable $dateHeure */
+    public function ModifRendezVous(string $id, string $praticienId, string $patientId, string $specialite, \DateTimeImmutable $dateHeure) : RdvDTO {
+        try {
+            // todo : test rdvId exists
+            // todo : test NEW praticienId exists
+
+            $rdv = $this->rdvRepository->getRDVById($id);
+            $praticien = $this->servicePraticien->getPraticienById($praticienID);
+
+            $rdvId = $rdv->$id;
+    
+            // TODO: prendre les infos de l'ancient rdv
+            $getSpecialite = $this->$rdvRepository->getSpecialite($specialite);
+            $getPatientId = $this->$rdvRepository->getPatientId($patientId);
+
+            
+            // todo: test spé avec praticien
+            if ($praticien->specialite_label = $getSpecialite) {
+                throw new \Exception($praticien->specialite_label."=".$getSpecialite);
+
+                // !: supp ancient rdv
+
+                $this->rdvRepository->delete($rdv);
+
+                // !: créer nouveau rdv avec RdvId et spé qui reste en changeant date, praticien
+    
+                $rdv = new RendezVous($rdvId, $praticien, $getPatientId, $getspecialite, $dateHeure);
+                $this->rdvRepository->addRdv($rdv);
+                return $rdv->toDTO($praticien);
+
+            }
+    
+        } catch(RepositoryEntityNotFoundException $e) {
+            throw new ServiceRDVInvalidDataException('RDV invalide' . $e->getMessage());
         } 
     }
 }
