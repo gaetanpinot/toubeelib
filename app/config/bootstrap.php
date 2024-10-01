b7ba68a2012447cca2ee86db5777633e49efe055
gaetanpinot <gaetan.pinot6@etu.univ-lorraine.fr>
2024-10-01 16:44:58 +0200

    debut cors

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
 use DI\ContainerBuilder;
+use Slim\Exception\HttpUnauthorizedException;
 use Slim\Factory\AppFactory;
 
 
@@ -17,10 +18,21 @@ $app = AppFactory::createFromContainer($c);
 
 $app->addBodyParsingMiddleware();
 $app->addRoutingMiddleware();
-$app->addErrorMiddleware($c->get('displayErrorDetails'), false, false)
+$app->addErrorMiddleware($c->get('displayErrorDetails'), false, false);
 //    ->getDefaultErrorHandler()
 //    ->forceContentType('application/json')
-;
+
+$app->add(function ($request, $handler) {
+    if (! $request->hasHeader('Origin'))
+throw new HttpUnauthorizedException ($request, "missing Origin Header (cors)");
+    $response = $handler->handle($request);
+    return $response
+        ->withHeader('Access-Control-Allow-Origin', 'localhost:6080')
+        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        ->withHeader('Access-Control-Max-Age', 3600)
+        ->withHeader('Access-Control-Allow-Credentials', 'true')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
 
 
 $app = (require_once __DIR__ . '/routes.php')($app);
