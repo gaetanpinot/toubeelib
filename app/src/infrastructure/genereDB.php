5c1ccc668db1ccaffb6836ef5aaea175c31cdc66
gaetanpinot <gaetan.pinot6@etu.univ-lorraine.fr>
2024-10-04 10:00:41 +0200

    generation des donnees

--- app/src/infrastructure/genereDB.php
+++ app/src/infrastructure/genereDB.php
@@ -1,42 +1,47 @@
 <?php
 
+require_once __DIR__ .'/../../vendor/autoload.php';
 $drop='drop table if exists patient,praticien,status,specialite,rdv;';
-$cpatient='
-create table patient(
-id UUID,
-nom varchar(50),
-prenom varchar(50),
-dateNaissance date,
-adresse varchar(100),
-tel varchar(15),
-primary key(id)
-);
-';
 $cspecialite='
 create table specialite(
 id varchar(5),
-label varchar(50),
-description text,
+label varchar(50) not null,
+description text not null,
 primary key(id)
 );
 ';
 $cstatus='
 create table status(
 id int,
-label varchar(50),
+label varchar(50) not null,
 primary key(id)
 );
 ';
 $cpraticien='
 create table praticien(
 id UUID,
-nom varchar(50),
-prenom varchar(50),
-adresse varchar(100),
-tel varchar(15),
-specialite varchar(5),
+rpps varchar(50) not null,
+nom varchar(50) not null,
+prenom varchar(50) not null,
+adresse varchar(100) not null,
+tel varchar(15) not null,
+specialite varchar(5) not null,
 primary key(id),
 foreign key(specialite) references specialite(id)
+);
+';
+$cpatient='
+create table patient(
+id UUID,
+nom varchar(50) not null,
+prenom varchar(50) not null,
+dateNaissance date not null,
+adresse varchar(100),
+tel varchar(20),
+mail varchar(100),
+idMedcinTraitant UUID,
+numSecuSociale varchar(50),
+primary key(id)
 );
 ';
 $crdv='
@@ -58,11 +63,76 @@ foreign key(status) references status(id)
 
 $co = new PDO('pgsql:host=toubeelib.db;port=5432;dbname=toubeelib;user=user;password=toto');
 $res=$co->exec($drop);
-$res=$co->exec($cpatient);
 $res=$co->exec($cspecialite);
 $res=$co->exec($cstatus);
 $res=$co->exec($cpraticien);
+$res=$co->exec($cpatient);
 $res=$co->exec($crdv);
+
+
+// specialite
+// id varchar(5),
+// label varchar(50) not null,
+// description text not null,
+// primary key(id)
+
+$spe = [
+	'A' => [
+		'ID' => 'A',
+		'label' => 'Dentiste',
+		'description' => 'Spécialiste des dents'
+	],
+	'B' => [
+		'ID' => 'B',
+		'label' => 'Ophtalmologue',
+		'description' => 'Spécialiste des yeux'
+	],
+	'C' => [
+		'ID' => 'C',
+		'label' => 'Généraliste',
+		'description' => 'Médecin généraliste'
+	],
+	'D' => [
+		'ID' => 'D',
+		'label' => 'Pédiatre',
+		'description' => 'Médecin pour enfants'
+	],
+	'E' => [
+		'ID' => 'E',
+		'label' => 'Médecin du sport',
+		'description' => 'Maladies et trausmatismes liés à la pratique sportive'
+	],
+];
+$query='insert into specialite (id, label, description) values (:ID, :label, :description);';
+$insert= $co->prepare($query);
+foreach($spe as $s){
+	$insert->execute($s);
+}
+
+
+
+
+//creation des patients
+// id UUID,
+// nom varchar(50),
+// prenom varchar(50),
+// dateNaissance date,
+// adresse varchar(100),
+// tel varchar(15),
+$faker = Faker\Factory::create();
+$patientIds=[];
+	$query="insert into patient (id, nom, prenom, dateNaissance, adresse, tel) values (:id,:nom,:prenom,:date,:adresse,:tel);";
+	$insert = $co->prepare($query);
+// for($i=0;$i<40;$i++){
+// 	$nom=$faker->name();
+// 	$prenom=$faker->name();
+// 	$date=$faker->date();
+// 	$adresse=$faker->address();
+// 	$tel=$faker->phoneNumber();
+// 	$id=$faker->uuid();
+// 	$insert->execute(['id'=>$id,'nom'=>$nom,'prenom'=>$prenom,'date'=>$date,'adresse'=>$adresse,'tel'=>$tel]);
+// 	$patientIds[]=$id;
+// }
 
 var_dump($res);
 // foreach($res as $r){
--- app/src/infrastructure/repositories/PgRdvRepository.php
+++ app/src/infrastructure/repositories/PgRdvRepository.php
@@ -4,6 +4,7 @@ namespace toubeelib\infrastructure\repositories;
 use toubeelib\core\domain\entities\rdv\RendezVous;
 use toubeelib\core\repositoryInterfaces\RdvRepositoryInterface;
 use PDO;
+use toubeelib\core\repositoryInterfaces\RepositoryEntityNotFoundException;
 
 class PgRdvRepository implements  RdvRepositoryInterface{
 
@@ -13,8 +14,23 @@ class PgRdvRepository implements  RdvRepositoryInterface{
     $this->pdo=$pdo;
 }
 
+// id UUID,
+// date timestamp,
+// patientId UUID,
+// praticienId UUID,
+// status int,
+// primary key(id),
     public function getRdvById(string $id): RendezVous
     {
+        $query='select * from rdv where id=:id;';
+        $statement=$this->pdo->prepare($query);
+        $statement->execute(['id'=>$id]);
+        $rdv=$statement->fetch();
+        if($rdv){
+            return new RendezVous($rdv['praticienId'],$rdv['patientId'],'spe',$rdv['date']);
+        }else{
+            throw new RepositoryEntityNotFoundException("Rendez vous $id n'existe pas");
+        }
     }
 
     public function addRdv(string $id, RendezVous $rdv): void
