93fde3c6bb7835beab0bad0441d20d1eb9d2ec52
LiamDespaquis <liamdespaqiis@gmail.com>
2024-10-20 20:19:39 +0200

    fin planning praticien

--- app/src/core/services/rdv/ServiceRDV.php
+++ app/src/core/services/rdv/ServiceRDV.php
@@ -102,6 +102,7 @@ class ServiceRDV implements ServiceRDVInterface {
     }
 
 <<<<<<< HEAD
+<<<<<<< HEAD
     public function getListeDisponibiliteDate(string $idPraticien, ?string $test_start_Date, ?string $test_end_Date): array
     {
         //echo "test for getListeDisponibiliteDate";
@@ -160,6 +161,9 @@ class ServiceRDV implements ServiceRDVInterface {
     }
 
     public function getRDVbyPatient(string $id) : array {
+=======
+    public function getRdvByPatient(string $id) : array {
+>>>>>>> 97bc7b966805fe9c16f222e9588a1fdcf7fcfdb8
 =======
     public function getRdvByPatient(string $id) : array {
 >>>>>>> 97bc7b966805fe9c16f222e9588a1fdcf7fcfdb8
