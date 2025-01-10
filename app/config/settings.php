4e690fd6ee1d2420ed332c651c22c1b1e4cbf5bf
Gaetan Pinot <gaetan.pinot6@etu.univ-lorraine.fr>
2025-01-10 14:46:20 +0100

    bruno

--- app/config/settings.php
+++ app/config/settings.php
@@ -2,7 +2,7 @@
 
 return  [
 
-    'displayErrorDetails' => false,
+    'displayErrorDetails' => true,
     'logs.dir' => __DIR__ . '/../var/logs',
     'db.config' => __DIR__.'/pdoConfig.ini',
     'auth.db.config' => __DIR__ . '/pdoConfigAuth.ini',
